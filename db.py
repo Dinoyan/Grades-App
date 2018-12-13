bdfc39de519a43b6e4061aa2943344029c4253a8
Dinoyan <dinoyan.g98@gmail.com>
2018-12-13 13:20:55 -0500

    Updated db client connection
    Made it private

--- db.py
+++ db.py
@@ -2,19 +2,24 @@ from pymongo import MongoClient
 import pymongo
 from bson.binary import Binary
 import pickle
+import db_client_info
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+myclient = db_client_info.myclient
 
-mydb = myclient["mydatabase"]
+mydb = myclient["grades"]
 
 students = mydb["students"]
 instructors = mydb["instructors"]
+
 
 def insertStudentUser(user):
 	ID = user.stuID
 	studentSerialize = pickle.dumps(user)
 	identifer = {}
 	identifer[ID] = studentSerialize
+
+	students.insert_one(identifer)
+
 	
 
 def usertInstructorUser(user):
