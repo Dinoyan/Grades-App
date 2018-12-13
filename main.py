9faedd5cf4ba4ff18bb635bbb0f739cc01ba0a3d
Dinoyan <dinoyan.g98@gmail.com>
2018-12-12 22:19:25 -0500

    New users and serialize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import base64
 import pymongo
+from db import *
+from bson.binary import Binary
+import pickle
 
 class Instructor:
 	def __init__(self, name, ID, username, password):
@@ -7,6 +10,9 @@ class Instructor:
 		self.ID = ID
 		sefl.username = username
 		self.password = base64.b64encoder(password)
+
+	def __str__():
+		return "Welcome " + self.name 
 
 	def enterMark(self):
 		pass
@@ -23,26 +29,45 @@ class Student():
 		self.name = name;
 		self.stuID = stuID;
 		self.username = username
-		self.password = password
+		self.password = base64.b64encoder(password)
 		self.marksList = {}
+
+	def __str__(self):
+		return "Welcome " + self.name
 
 	def viewMarks(self):
 		for name, mark in self.marksList.items():
 			print (name, "->", mark)
 
 
-
 if __name__ =="__main__":
-	print("Course Marks")
+	print("Course Marks\n")
 	username = raw_input("Enter username ")
 	password = raw_input("Enter password ")
 
-	if(username == "admin"):
-		print("Create New Account")
-		type = raw_input("Student or Instructor")
 
-		if (type == "Student"):
-			pass
-		else:
-			pass
+	# Creating new accounts
+	if(username == "admin"):
+	print("Create New Account\n")
+	type = raw_input("Student or Instructor")
+	if (type == "Student"):
+		name = raw_input("Enter name: ")
+		ID = raw_input("Enter ID: ")
+		username = raw_input("Enter username: ")
+		password = raw_input("Enter password: ")
+		Student = Student(name, ID, username, password)
+
+		studentSerialize = pickle.dumps(Student)
+		db.ininsertStudentUser(studentSerialize)
+	else:
+		pass
+
+
+	auth = db.authenticate(username, password)
+
+	if (auth == True):
+		pass
+	else:
+		pass
+
 
